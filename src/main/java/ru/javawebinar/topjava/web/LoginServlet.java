556f8c564c37286a33cbe60287853a8ef43fdda6
Aleksandr N Potashev <anpotashev@hotmail.com>
2017-11-10 12:55:28 +0300

    HW02 Change sendRedirect("/meals"); to sendRedirect("meals");

--- src/main/java/ru/javawebinar/topjava/web/LoginServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/LoginServlet.java
@@ -14,7 +14,7 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("userId"));
         AuthorizedUser.setId(id);
-        response.sendRedirect("/meals");
+        response.sendRedirect("meals");
     }
 
 }
