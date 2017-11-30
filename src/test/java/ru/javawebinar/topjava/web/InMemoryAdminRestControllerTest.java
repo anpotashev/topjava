8f6197277f2356366b42422ea222dd18392a6312
Aleksandr N Potashev <anpotashev@hotmail.com>
2017-11-30 17:25:52 +0300

    HW04 move 2 files to test

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web;
 
+import org.junit.*;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.UserTestData;
